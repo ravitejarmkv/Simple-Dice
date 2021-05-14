cdae869c63f88b67a7c8aaafe79979d06722d9f4
ravitejarmkv <ravitejarmkv@github.com>
2021-05-14 10:43:54 +0530

    Simple Dice to play

---
+++ Single_player.py
@@ -0,0 +1,12 @@
+import random
+
+roll_again = input("Do you want to roll? ")
+res = []
+while roll_again == "y" or roll_again == 'yes':
+    s = random.randint(1, 6)
+    res.append(s)
+    print([s])
+    roll_again = input("Do you want to roll again? ")
+print("Sum of rolled numbers: ", sum(res))
+print("rolled  numbers", res)
+print("game has end!")
